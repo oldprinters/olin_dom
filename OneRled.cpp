dbcf611b73967430259650958d0f8357b6314791
Ivanych <zakaz@oldprinters.ru>
2020-11-27 10:35:48 +0300

    Сделал "более контрастным" вывод на светодиоды в автоматическом режиме.

--- OneRled.cpp
+++ OneRled.cpp
@@ -7,7 +7,11 @@
 void OneRled::cycle()
 {
 	if (rTimer.getTimer()) {
-		setDim(random(155));
+		int e = random(10);
+		e *= e;
+		e *= (-1 * random(0,2) ? 1 : -1);
+		e += 100;
+		setDim(e);
 		rTimer.setTimer(random(2000, 10000));
 	}
 	OneLed::cycle();
